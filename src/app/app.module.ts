aacda704f7831eaaaeee346dfcc05f715ce493c7
SagarBagga <kartiksagar007@gmail.com>
2019-08-04 19:24:35 +0800

    bootstrap 3 added for header, footer and test component- jquery not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TestComponent } from './test/test.component';
 import { ServerComponent } from './server/server.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent, ServerComponent ],
+  declarations: [ AppComponent, HelloComponent, TestComponent, ServerComponent, HeaderComponent, FooterComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
