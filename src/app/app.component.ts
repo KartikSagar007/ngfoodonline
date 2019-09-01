f95d515bb0df3018de37b614603c4b1fd5b16953
Baga <sagbaga@apac.corpdir.net>
2019-09-02 00:00:55 +0800

    entire project renewed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Hungry Guys';
-  result="29";
+export class AppComponent {
+  title = 'poc';
 }
